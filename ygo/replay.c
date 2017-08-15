4da9ad937d208aa05265a4faa554cf2b50ef3853
jonas-git <jonas.vanen@gmail.com>
2017-08-15 16:44:21 +0200

    implementation of ygo_replay_fread - replaced previous use of calloc with malloc - properly handling errors with gotos

--- ygo/replay.c
+++ ygo/replay.c
@@ -23,8 +23,34 @@ int ygo_replay_read(struct ygo_replay *replay, const char *filename)
 
 int ygo_replay_fread(struct ygo_replay *replay, const FILE *stream)
 {
-	fputs(stderr, "ygo_replay_fread: not implemented");
-	return -1;
+	int err = 0;
+
+	if (fseek(stream, 0, SEEK_END) != 0) {
+		err = YGO_REPLAY_ERR_FSEEK;
+		goto err_fseek;
+	}
+	long size = ftell(stream);
+	rewind(stream);
+
+	char *buf = malloc((size + 1) * sizeof(char));
+	if (!buf) {
+		err = YGO_REPLAY_ERR_MALLOC;
+		goto err_malloc;
+	}
+
+	size_t nread = fread(buf, sizeof(char), size, stream);
+	if (nread != size && ferror(stream) != 0) {
+		err = YGO_REPLAY_ERR_FREAD;
+		goto err_fread;
+	}
+
+	err = ygo_replay_sread(replay, buf, nread);
+
+err_fread:
+	free(buf);
+err_malloc:
+err_fseek:
+	return err;
 }
 
 int ygo_replay_sread(struct ygo_replay *replay, const char *data, size_t size)
