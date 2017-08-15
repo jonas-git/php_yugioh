07e7def97d7ba17b16bb3d113e51c91c0490b997
jonas-git <jonas.vanen@gmail.com>
2017-08-15 16:08:13 +0200

    implementation of ygo_replay_read

--- ygo/replay.c
+++ ygo/replay.c
@@ -1,3 +1,30 @@
+#include <stdio.h>
 #include "replay.h"
 
+int ygo_replay_read(struct ygo_replay *replay, const char *filename)
+{
+	FILE *stream = fopen(filename, "rb");
+	if (!stream)
+		return YGO_REPLAY_ERR_FOPEN;
 
+	int err = ygo_replay_fread(replay, stream);
+	if (err != YGO_REPLAY_ERR_OK)
+		return err;
+
+	if (fclose(stream) == EOF)
+		return YGO_REPLAY_ERR_FCLOSE;
+
+	return 0;
+}
+
+int ygo_replay_fread(struct ygo_replay *replay, const FILE *stream)
+{
+	fputs(stderr, "ygo_replay_fread: not implemented");
+	return -1;
+}
+
+int ygo_replay_sread(struct ygo_replay *replay, const char *data, size_t size)
+{
+	fputs(stderr, "ygo_replay_sread: not implemented");
+	return -1;
+}
