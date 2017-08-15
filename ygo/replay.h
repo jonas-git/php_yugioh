37a5c24f89a347ba98016012c61fe1ec2721fde6
jonas-git <jonas.vanen@gmail.com>
2017-08-15 15:29:10 +0200

    added comments containing high-level information ... about the structs and functions of the new interface. Additional information about the changes that were made: - added detailed comments - changed card ID type (in decks) to unsigned - using static memory where possible

--- ygo/replay.h
+++ ygo/replay.h
@@ -9,6 +9,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// holds meta-information about a Yu-Gi-Oh! replay.
 struct ygo_replay_header
 {
 	uint32_t id;
@@ -20,6 +21,8 @@ struct ygo_replay_header
 	unsigned char props[YGO_REPLAY_PROPS_SIZE];
 };
 
+// holds information about a single player in a replay
+// including his name and the cards in the deck he played.
 struct ygo_replay_deck
 {
 	char *owner;
@@ -32,6 +35,8 @@ struct ygo_replay_deck
 	unsigned int size_extra : 4;
 };
 
+// holds general information about a yugioh duel (in a replay).
+// this does not contain the progress and the result of it.
 struct ygo_replay
 {
 	struct ygo_replay_deck decks[YGO_REPLAY_NUM_PLAYERS_MAX];
@@ -42,8 +47,32 @@ struct ygo_replay
 	int32_t draw_count;
 };
 
+/* ygo_replay_read(replay, filename):
+ |     opens the specified file `filename`, parses its content and writes
+ |     the result into the [struct ygo_replay] at the address of `replay`.
+ |  -> returns an error code that indicates how the function terminated
+ | replay:     pointer to a [struct ygo_replay]
+ | filename:   path to a compressed replay file
+ */
 int ygo_replay_read(struct ygo_replay *replay, const char *filename);
+
+/* ygo_replay_fread(replay, stream):
+ |     parses the content of the file `stream` and writes the result
+ |     into the [struct ygo_replay] at the address of `replay`.
+ |  -> returns an error code that indicates how the function terminated
+ | replay:   pointer to a [struct ygo_replay]
+ | stream:   pointer to an opened [FILE] containing compressed replay data
+ */
 int ygo_replay_fread(struct ygo_replay *replay, const FILE *stream);
+
+/* ygo_replay_sread(replay, data, size):
+ |     parses the passed `data` and writes the result
+ |     into the [struct ygo_replay] at the address of `replay`.
+ |  -> returns an error code that indicates how the function terminated
+ | replay:   pointer to a [struct ygo_replay]
+ | data:     string of compressed replay data
+ | size:     size of the data
+ */
 int ygo_replay_sread(struct ygo_replay *replay, const char *data, size_t size);
 
 #endif /* YGO_REPLAY_H */
